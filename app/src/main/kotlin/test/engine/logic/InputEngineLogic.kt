4a6fa1ae7222e608dca8ebbd1d5af3e76eadd5cd
Stanley Wintergreen <kepocnhh@protonmail.com>
2024-08-24 23:18:49 +0300

    input logic

--- app/src/main/kotlin/test/engine/logic/InputEngineLogic.kt
+++ app/src/main/kotlin/test/engine/logic/InputEngineLogic.kt
@@ -3,11 +3,15 @@ package test.engine.logic
 import sp.kx.lwjgl.engine.Engine
 import sp.kx.lwjgl.engine.EngineInputCallback
 import sp.kx.lwjgl.engine.EngineLogic
+import sp.kx.lwjgl.engine.input.Keyboard
 import sp.kx.lwjgl.entity.Canvas
 import sp.kx.lwjgl.entity.Color
 import sp.kx.lwjgl.entity.input.KeyboardButton
 import sp.kx.math.Point
 import sp.kx.math.measure.frequency
+import sp.kx.math.measure.measureOf
+import sp.kx.math.pointOf
+import sp.kx.math.sizeOf
 import test.engine.logic.util.FontInfoUtil
 
 internal class InputEngineLogic(private val engine: Engine) : EngineLogic {
@@ -28,14 +32,48 @@ internal class InputEngineLogic(private val engine: Engine) : EngineLogic {
         }
     }
 
+    private val measure = measureOf(24.0)
+
+    private fun onRenderKeyboard(canvas: Canvas, x: Double, y: Double, keyboard: Keyboard) {
+        val width = 1.0
+        setOf(
+            setOf(KeyboardButton.Q, KeyboardButton.W, KeyboardButton.E, KeyboardButton.R, KeyboardButton.T, KeyboardButton.Y, KeyboardButton.U, KeyboardButton.I, KeyboardButton.O, KeyboardButton.P),
+            setOf(KeyboardButton.A, KeyboardButton.S, KeyboardButton.D, KeyboardButton.F, KeyboardButton.G, KeyboardButton.H, KeyboardButton.J, KeyboardButton.K, KeyboardButton.L),
+            setOf(KeyboardButton.Z, KeyboardButton.X, KeyboardButton.C, KeyboardButton.V, KeyboardButton.B, KeyboardButton.N, KeyboardButton.M)
+        ).forEachIndexed { dY, row ->
+            row.forEachIndexed { dX, button ->
+                val isPressed = keyboard.isPressed(button)
+                val pointTopLeft = pointOf(x + width * dX, y + width * dY)
+                canvas.texts.draw(
+                    info = FontInfoUtil.getFontInfo(height = 1.0, measure = measure),
+                    color = if (isPressed) Color.YELLOW else Color.GREEN,
+                    pointTopLeft = pointTopLeft,
+                    text = button.name,
+                    measure = measure,
+                )
+                if (isPressed) {
+                    canvas.polygons.drawRectangle(
+                        color = Color.YELLOW,
+                        pointTopLeft = pointOf(x + width * dX - width * 0.25, y + width * dY),
+                        size = sizeOf(width = width, height = width),
+                        lineWidth = 0.1,
+                        measure = measure,
+                    )
+                }
+            }
+        }
+    }
+
     override fun onRender(canvas: Canvas) {
         val fps = engine.property.time.frequency()
         canvas.texts.draw(
-            info = FontInfoUtil.getFontInfo(height = 16f),
+            info = FontInfoUtil.getFontInfo(height = 1.0, measure = measure),
             pointTopLeft = Point.Center,
             color = Color.GREEN,
             text = String.format("%.2f", fps),
+            measure = measure,
         )
+        onRenderKeyboard(canvas = canvas, x = 1.0, y = 1.0, engine.input.keyboard)
     }
 
     override fun shouldEngineStop(): Boolean {
