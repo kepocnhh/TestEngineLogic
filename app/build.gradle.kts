a182e70fd261b0e5b9a7dd2ce74308452be2ee26
Stanley Wintergreen <kepocnhh@protonmail.com>
2024-11-21 18:12:22 +0300

    0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
